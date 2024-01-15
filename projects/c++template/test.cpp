475facbdf70eb383a49354a0859629b70c8d0012
Nauman Chaudhry <naumanch969@gmail.com>
2024-01-15 17:42:13 +0500

    Maze in cpp

--- projects/c++template/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    auto result = 0;
-    std::cout << result << std::endl;
-} 
--- sdl/Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
